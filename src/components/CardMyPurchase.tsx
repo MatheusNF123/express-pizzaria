5b1420d895a96f09dae9b7c36f135128a0f4100a
Matheus <matheus_cs.nf@hotmail.com>
2023-01-11 23:21:00 -0300

    styling component cardMypurchase

--- src/components/CardMyPurchase.tsx
+++ src/components/CardMyPurchase.tsx
@@ -22,49 +22,57 @@ export default function CardMyPurchase({ info }: CartCardProps) {
   const router = useRouter();
 
   return (
-    <Box sx={{backgroundColor: 'white', padding: '10px 0px'}}>
+    <Card elevation={0} sx={{display: 'flex'} }>
        <Container maxWidth="xl">
-       <Typography sx={{marginBottom: '5px'}} variant="h6">3 de dezembro de 2022 </Typography>
+    <Box sx={{display: "flex", flexDirection: "column"}}>
+       <Typography sx={{m: 1}} component="div">3 de dezembro de 2022 </Typography>
     <Divider />
-    <Card elevation={0} sx={{display: 'flex', /* alignItems: 'center' */} }>
-      <CardMedia
-      sx={{
-        width: "80px",
-        height: "80px",
-        border: "2px solid black",
-        // margin: "20px 5px 20px 20px",
-      }}
-        // image={img}
-        title="Pizza Image"
-      />
-      <CardContent>
-        <Typography variant="h6"> pizza de qualquer coisa{info}</Typography>
-        <Typography>Quantidate</Typography>
-      </CardContent>
-      <CardActions
-       sx={{
-        display: "flex",        
-      }}>
-        <Box
-            sx={{
-              display: "flex",
-              flexDirection: 'column', 
-              gap: "4px",
-            }}>
-        <Button variant="contained" onClick={() => router.push("/")}>
-          ver compra
-        </Button>
-        <Button
-          variant="contained"              
-          onClick={() => router.push(`/`)}
-          >
-          comprar novamente
-        </Button>
+      <CardContent sx={{display: 'flex', width: '100%'}}>
+        <Box sx={{mr: 1}}>
+        <CardMedia
+        sx={{
+          width: "100px",
+          height: "100px",
+          border: "2px solid black",
+          // margin: "20px 5px 20px 20px",
+        }}
+          // image={img}
+          title="Pizza Image"
+        />
         </Box>
-      </CardActions>
+        <Box sx={{flex: 1}}>
+        <Typography variant="h6" component="div" sx={{mb: 1}}>pizza de qualquer coisa {info}</Typography>
+        <Typography variant="subtitle2">Quantidate: 2</Typography>
+        <Typography variant="subtitle2">Price uni: 29,90</Typography>
+        </Box>
+        <Box sx={{flex: 1}}>
+        <Typography component="span">Status: Entregue</Typography>
+        </Box>
+        <CardActions
+        sx={{
+          display: "flex",        
+        }}>
+          <Box
+              sx={{
+                display: "flex",
+                flexDirection: 'column', 
+                gap: "4px",
+              }}>
+          <Button variant="contained" onClick={() => router.push("/")}>
+            ver compra
+          </Button>
+          <Button
+            variant="contained"              
+            onClick={() => router.push(`/`)}
+            >
+            comprar novamente
+          </Button>
+          </Box>
+        </CardActions>
+      </CardContent>
      
-    </Card>
-    </Container>
     </Box>
+    </Container>
+    </Card>
   );
 }
