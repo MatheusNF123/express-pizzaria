3c7d18d3d8448ecfba1b7c71135af7dc688d5715
Matheus <matheus_cs.nf@hotmail.com>
2023-04-11 17:24:49 -0300

    styling the Admin User Page

--- pages/admin/usuarios.tsx
+++ pages/admin/usuarios.tsx
@@ -1,19 +1,170 @@
 import { useState } from "react";
 import { GetServerSideProps } from "next";
-import { Box, Button, Container, Grid, Stack, Typography } from "@mui/material";
+import {
+  Box,
+  Container,
+  Typography,
+  Table,
+  TableBody,
+  TableCell,
+  tableCellClasses,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Avatar,
+  IconButton,
+} from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
 
-import { getRequest } from "../../src/services/api";
+import { getRequest, deleteRequest } from "../../src/services/api";
 import { User } from "../../src/Types";
 import Layout from "../../src/components/layout";
 import setApiHeaders from "../../src/services/setApiHeaders";
-import AdminUserCard from "../../src/components/adminUserCard";
+
+import { styled } from "@mui/material/styles";
+
+const tableHeader = ["Foto", "Nome", "Email", "Endereço", "Telefone", ""];
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: "#0000005c",
+    color: "white",
+    fontWeight: "bold",
+    fontSize: 20,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 18,
+    color: "white",
+  },
+  border: 0,
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  "&:nth-of-type(odd)": {
+    backgroundColor: "#3131315c",
+  },
+  // hide last border
+  "&:last-child td, &:last-child th": {
+    border: 0,
+  },
+}));
 
 type AdminUsersProps = {
   users: User[] | null;
 };
+const rows: User[] | null = [
+  {
+    id: "1",
+    name: "fsdfsd",
+    address: "dsfdsf",
+    email: "fsdfsd",
+    phone: "912341234",
+    img: "https://pbs.twimg.com/profile_images/1569051266877112322/5cGtNQZf_400x400.jpg",
+    role: "",
+  },
+  {
+    id: "2",
+    name: "fsdfsd",
+    address: "dsfdsf",
+    email: "fsdfsd",
+    phone: "912341234",
+    img: "",
+    role: "",
+  },
+  {
+    id: "3",
+    name: "fsdfsd",
+    address: "dsfdsf",
+    email: "fsdfsd",
+    phone: "912341234",
+    img: "https://pbs.twimg.com/profile_images/1569051266877112322/5cGtNQZf_400x400.jpg",
+    role: "",
+  },
+  {
+    id: "4",
+    name: "fsdfsd",
+    address: "dsfdsf",
+    email: "fsdfsd",
+    phone: "912341234",
+    img: "",
+    role: "",
+  },
+  {
+    id: "5",
+    name: "fsdfsd",
+    address: "dsfdsf",
+    email: "fsdfsd",
+    phone: "912341234",
+    img: "https://pbs.twimg.com/profile_images/1569051266877112322/5cGtNQZf_400x400.jpg",
+    role: "",
+  },
+  {
+    id: "6",
+    name: "fsdfsd",
+    address: "dsfdsf",
+    email: "fsdfsd",
+    phone: "912341234",
+    img: "",
+    role: "",
+  },
+  {
+    id: "7",
+    name: "fsdfsd",
+    address: "dsfdsf",
+    email: "fsdfsd",
+    phone: "912341234",
+    img: "",
+    role: "",
+  },
+  {
+    id: "8",
+    name: "fsdfsd",
+    address: "dsfdsf",
+    email: "fsdfsd",
+    phone: "912341234",
+    img: "",
+    role: "",
+  },
+  {
+    id: "9",
+    name: "fsdfsd",
+    address: "dsfdsf",
+    email: "fsdfsd",
+    phone: "912341234",
+    img: "",
+    role: "",
+  },
+  {
+    id: "10",
+    name: "fsdfsd",
+    address: "dsfdsf",
+    email: "fsdfsd",
+    phone: "912341234",
+    img: "",
+    role: "",
+  },
+  {
+    id: "11",
+    name: "fsdfsd",
+    address: "dsfdsf",
+    email: "fsdfsd",
+    phone: "912341234",
+    img: "",
+    role: "",
+  },
+  {
+    id: "12",
+    name: "fsdfsd",
+    address: "dsfdsf",
+    email: "fsdfsd",
+    phone: "912341234",
+    img: "",
+    role: "",
+  },
+];
 
 export default function AdminUsers(props: AdminUsersProps) {
-  const [users, setUsers] = useState<User[] | null>(props.users);
+  const [users, setUsers] = useState<User[] | null>(rows);
 
   const handleUsersReload = async () => {
     try {
@@ -28,36 +179,77 @@ export default function AdminUsers(props: AdminUsersProps) {
     }
   };
 
+  const handleUserDeletion = async (id: string) => {
+    setApiHeaders();
+    await deleteRequest(`admin/user/${id}`);
+
+    await handleUsersReload();
+  };
+
   return (
     <Layout title="Admin: usuários">
-      <Box
-        sx={{
-          minHeight: "calc(100vh - 100px)",
-          width: "100%",
-          display: "flex",
-          justifyContent: "center",
-          padding: "20px",
-        }}
-      >
+      <Container maxWidth="xl">
+        <Typography color="white" variant="h4" sx={{ m: 2 }}>
+          Controle de Usuários
+        </Typography>
         <Box
           sx={{
-            color: "white",
-            display: "flex",
-            flexDirection: "column",
-            gap: "10px",
-            padding: "20px",
+            minHeight: "calc(100vh - 100px)",
             width: "100%",
+            pb: 10,
           }}
         >
-          {users?.map((user) => (
-            <AdminUserCard
-              key={user.id}
-              user={user}
-              handleUsersReload={handleUsersReload}
-            />
-          ))}
+          <TableContainer
+            sx={{
+              backgroundColor: "#00000034",
+              borderRadius: "10px",
+            }}
+          >
+            <Table sx={{ minWidth: 700 }} aria-label="Table de usuários">
+              <TableHead>
+                <TableRow>
+                  {tableHeader.map((col, i) => (
+                    <StyledTableCell key={i}>{col}</StyledTableCell>
+                  ))}
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {users?.map((row) => (
+                  <StyledTableRow key={row.name}>
+                    <StyledTableCell>
+                      <Avatar
+                        sx={{
+                          width: 50,
+                          height: 50,
+                          backgroundColor: "white",
+                          border: "2px solid #FFCC33",
+                        }}
+                        title={`Imagem do ${row.name} `}
+                        src={row.img}
+                      />
+                    </StyledTableCell>
+                    <StyledTableCell> {row.name} </StyledTableCell>
+                    <StyledTableCell>{row.email}</StyledTableCell>
+                    <StyledTableCell>{row.address}</StyledTableCell>
+                    <StyledTableCell>{row.phone}</StyledTableCell>
+                    <StyledTableCell align="right">
+                      <IconButton
+                        aria-label="Excluir usuário"
+                        onClick={() => handleUserDeletion(row.id)}
+                      >
+                        <DeleteIcon
+                          fontSize="large"
+                          sx={{ color: "white", fontSize: 30 }}
+                        />
+                      </IconButton>
+                    </StyledTableCell>
+                  </StyledTableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
         </Box>
-      </Box>
+      </Container>
     </Layout>
   );
 }
