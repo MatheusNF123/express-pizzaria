806afeaa936238de5bec22b7e02d65e60bd5f05e
Matheus <matheus_cs.nf@hotmail.com>
2023-04-19 18:17:22 -0300

    styling the home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,66 +1,139 @@
-import { Box, Button, Container } from "@mui/material";
+import { Box, Button, Typography } from "@mui/material";
+import { Bungee_Inline } from "@next/font/google";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import { useRouter } from "next/router";
+import Head from "next/head";
 import Image from "next/image";
-// import Image from "next/image";
+import pizza from "../public/pizza.png";
 
-// import pizzaBanner from "../src/images/pizzaBanner.png";
-import Header from "../src/components/header";
-import Layout from "../src/components/layout";
-// import pizzariaBanner from "../src/images/pizzariaBanner.png";
+export const bungeeInline = Bungee_Inline({
+  weight: ["400"],
+  subsets: ["latin"],
+  display: "swap",
+  fallback: ["Helvetica", "Arial", "sans-serif"],
+});
 
 export default function Home() {
-
+  const router = useRouter();
   return (
-    <Layout title="Home">
+    <>
+      <Head>
+        <title>Home</title>
+      </Head>
       <Box
-        // maxWidth="xl"
         sx={{
-          minHeight: "calc(100vh - 100px)",
-          width: "100%",
+          background:
+            "radial-gradient(circle, rgba(48,48,48,1) 0%, rgba(0,0,0,1) 100%)",
+          color: "white",
           display: "flex",
-          justifyContent: "center",
-          // padding: "20px",
+          flexDirection: "column",
+          height: "100vh",
+          width: "100vw",
+          position: "relative",
+          overflow: "hidden",
         }}
       >
-        <Box sx={{
-          // backgroundColor: "white",
-          backgroundImage: `url(/pizzariaBanner.png)`,
-          backgroundRepeat: 'no-repeat',
-          // objectFit: "scale-down",
-          // backgroundSize: "fill",
-          width: "100%",
-          // height: "calc(100vh - 70px)",
-          backgroundSize: "100% 100%",
-          backgroundPosition: "center center",
-        }}></Box>
-        {/* <Image style={{ width: "100%", height: "100vh" }} src={pizzariaBanner} alt="Banner da pizzaria" /> */}
+        <Box
+          sx={{
+            alignItems: "center",
+            display: "flex",
+            flexDirection: "column",
+            height: "100%",
+            width: "85%",
+            justifyContent: "center",
+          }}
+        >
+          <Typography
+            variant="h1"
+            sx={{
+              color: "primary.main",
+              fontFamily: bungeeInline.style.fontFamily,
+              fontSize: "6rem",
+              mb: 5
+            }}
+          >
+            Express pizzaria
+          </Typography>
+          <Typography sx={{ margin: "60px 0 40px 0" }} variant="h3">
+            Venha conhecer nossa pizzaria
+          </Typography>
+          <Button
+            onClick={() => router.push("/pizzas")}
+            variant="contained"
+            sx={{
+              // width: "200px",
+              fontWeight: "bold",
+              padding: "15px 100px",
+              fontSize: "25px"
+            }}
+          >
+            Ir para o site
+          </Button>
+        </Box>
+        <Box
+          sx={{
+            padding: "10px",
+            width: "100%",
+          }}
+        >
+          <Box
+            sx={{
+              alignItems: "center",
+              display: "flex",
+              gap: "5px",
+            }}
+          >
+            <GitHubIcon fontSize="medium" />
+            <Typography
+              href="https://github.com/Pedro-28"
+              target="_blank"
+              component="a"
+              variant="h5"
+              sx={{
+                color: "primary.main",
+                textDecoration: "none",
+                "&:hover": { color: "#B28E23" },
+              }}
+            >
+              github.com/Pedro-28
+            </Typography>
+          </Box>
+          <Box
+            sx={{
+              alignItems: "center",
+              display: "flex",
+              gap: "5px",
+            }}
+          >
+            <GitHubIcon fontSize="medium" />
+            <Typography
+              href="https://github.com/MatheusNF123"
+              target="_blank"
+              component="a"
+              variant="h5"
+              sx={{
+                color: "primary.main",
+                textDecoration: "none",
+                "&:hover": { color: "#B28E23" },
+              }}
+            >
+              github.com/MatheusNF123
+            </Typography>
+          </Box>
+        </Box>
+        <Image
+          style={{
+            position: "absolute",
+            right: -650,
+            bottom: -700,
+          }}
+          alt="Pizza Logo"
+          src={pizza}
+          width="1400"
+          height="1200"
+        />
+
       </Box>
-    </Layout>
-    // <>
-    //   <Head>
-    //     <title>Home</title>
-    //   </Head>
-    //   <main>
-    //     <Header />
-    //     <Box
-    //       sx={{            
-    //         display: "flex",
-    //         width: "100%",
-    //         height: "100vh",
-    //       }}
-    //     >
-    //       <Button
-    //         sx={{
-    //           color: "white",
-    //           borderColor: "white",
-    //           display: "flex",
-    //           alignSelf: "end",
-    //         }}
-    //         variant="outlined"
-    //       >
-    //         <Link href={'/pizzas'}>Menu</Link>
-    //       </Button>
-    //     </Box>
-    //   </main>
-    // </>
+    </>
   );
 }
